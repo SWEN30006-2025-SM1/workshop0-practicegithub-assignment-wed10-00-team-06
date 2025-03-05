b617f49a4c74b540112c780d622ccd4c0b70267e
weianh <benny.huang@student.unimelb.edu.au>
2025-03-05 11:15:46 +1100

    test

--- app/src/main/java/example/Calculator.java
+++ app/src/main/java/example/Calculator.java
@@ -3,7 +3,7 @@ package example;
 public class Calculator {
     public int add(int a, int b) {
         // TODO: Implement this method
-        return 1;
+        return 0;
     }
 
     public int minus(int a, int b) {
