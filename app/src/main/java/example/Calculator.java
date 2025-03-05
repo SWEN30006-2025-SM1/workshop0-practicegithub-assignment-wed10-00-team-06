f31f21c15d86aab849d3ba643d699aeacab1b5fe
Daqlan <daqlanl@student.unimelb.edu.au>
2025-03-05 11:07:44 +1100

    test

--- app/src/main/java/example/Calculator.java
+++ app/src/main/java/example/Calculator.java
@@ -3,21 +3,21 @@ package example;
 public class Calculator {
     public int add(int a, int b) {
         // TODO: Implement this method
-        return 4;
+        return 1;
     }
 
     public int minus(int a, int b) {
         // TODO: Implement this method
-        return 10;
+        return 1;
     }
 
     public int multiply(int a, int b) {
         // TODO: Implement this method
-        return 10;
+        return 1;
     }
 
     public double divide(int a, int b) {
         // TODO: Implement this method
-        return 5;
+        return 1;
     }
 }
